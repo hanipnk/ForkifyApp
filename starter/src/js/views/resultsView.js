6cc2110c9e7c743fb166edfe442f02bbcb15c840
hanipnk <hanipnk@gmail.com>
2021-07-26 06:12:00 -0400

    Error handling

--- starter/src/js/views/resultsView.js
+++ starter/src/js/views/resultsView.js
@@ -15,7 +15,7 @@ class ResultsView extends View {
     <li class="preview">
       <a class="preview__link" href="#${result.id}">
         <figure class="preview__fig">
-          <img src="${result.image}" alt="Test" crossorigin/>
+          <img src="${result.image}" alt="${result.title}" crossorigin/>
         </figure>
         <div class="preview__data">
           <h4 class="preview__title">${result.title}</h4>
