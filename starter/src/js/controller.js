a18f1f2e5c343dbdd6825c3c357f6de721297fd2
hanipnk <hanipnk@gmail.com>
2021-08-01 13:16:27 -0400

    Storing bookmarks with local storage

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -25,15 +25,18 @@ const controlRecipes = async function () {
 
     // 0) update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    // 1) Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    // 1) Loading Recipe
+    // 2) Loading Recipe
     await model.loadRecipe(id);
 
-    // 2) Rendering recipe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
@@ -89,7 +92,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdataServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
